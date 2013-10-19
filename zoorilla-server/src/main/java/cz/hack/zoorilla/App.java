1cd8f1adfc4cdd6485a89aa342d775b53b4d838b
pcipov <pcipov@kerio.com>
2013-10-19 15:20:33 +0200

    server is now service that can be started and stopped

--- zoorilla-server/src/main/java/cz/hack/zoorilla/App.java
+++ zoorilla-server/src/main/java/cz/hack/zoorilla/App.java
@@ -1,6 +1,5 @@
 package cz.hack.zoorilla;
 
-import cz.hack.zoorilla.notify.NotificationServlet;
 import com.google.common.base.Charsets;
 import cz.hack.zoorilla.notify.NotificationBridge;
 import cz.hack.zoorilla.notify.NotificationBroker;
@@ -9,44 +8,35 @@ import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.RetryNTimes;
 import org.apache.curator.test.TestingServer;
 import org.apache.zookeeper.CreateMode;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.session.HashSessionManager;
-import org.eclipse.jetty.server.session.SessionHandler;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class App {
 	
 	private static final Logger logger = LoggerFactory.getLogger(App.class);
-	private static final int PORT = 8080;
 
     public static void main(String[] args) throws Exception {
         TestingServer zooServer = new TestingServer(2181);
-		
 		
         CuratorFramework client = CuratorFrameworkFactory.newClient("localhost:2181", new RetryNTimes(Integer.MAX_VALUE, 1000));
         client.start();
         client.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT).forPath("/a/b/c", "xxx".getBytes(Charsets.UTF_8));
         
 		
-		
 		NotificationBroker w = new NotificationBroker(client);
 		NotificationBridge bridge = new NotificationBridge(client, w);
         
-        Server server = new Server(PORT);
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setSessionHandler(new SessionHandler(new HashSessionManager()));
-        context.addServlet(new ServletHolder(new NodeServlet(client)), "/0/node/*");
-        context.addServlet(new ServletHolder(new ChildrenServlet(client)), "/0/children/*");
-		context.addServlet(new ServletHolder(new NotificationServlet(w)), "/0/notify");
-        context.setContextPath("/");
-        server.setHandler(context);
+		ServerService server = new ServerService(client, w);
         server.start();
-		logger.info("Zoorilla started on port "+PORT);
-        server.join();
 		
+		logger.info("Zoorilla started on port "+server.getPort());
+        
+		System.out.println("Press any key ");
+		System.in.read();
+		
+		server.stop();
+		client.close();
+		zooServer.stop();
 		
     }
 }
---
+++ zoorilla-server/src/main/java/cz/hack/zoorilla/ServerService.java
@@ -0,0 +1,62 @@
+package cz.hack.zoorilla;
+
+import cz.hack.zoorilla.notify.NotificationBroker;
+import cz.hack.zoorilla.notify.NotificationServlet;
+import org.apache.curator.framework.CuratorFramework;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.session.HashSessionManager;
+import org.eclipse.jetty.server.session.SessionHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+/**
+ *
+ * @author pcipov
+ */
+public class ServerService {
+	
+	private static final int PORT = 8080;
+	
+	private final CuratorFramework client;
+	private final NotificationBroker broker;
+	private final int port;
+	
+	private Server server;
+	
+	public ServerService(CuratorFramework client, NotificationBroker broker) {
+		this(client, broker, PORT);
+	}
+
+	public ServerService(CuratorFramework client, NotificationBroker broker, int port) {
+		this.client = client;
+		this.broker = broker;
+		this.port = port;
+	}
+	
+	public void start() throws Exception {
+		if(server != null) {
+			throw new IllegalStateException("Server is already running");
+		}
+		
+		server = new Server(PORT);
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setSessionHandler(new SessionHandler(new HashSessionManager()));
+        context.addServlet(new ServletHolder(new NodeServlet(client)), "/0/node/*");
+        context.addServlet(new ServletHolder(new ChildrenServlet(client)), "/0/children/*");
+		context.addServlet(new ServletHolder(new NotificationServlet(broker)), "/0/notify");
+        context.setContextPath("/");
+        server.setHandler(context);
+        server.start();
+	}
+	
+	public void stop() throws Exception {
+		if (server != null) {
+			server.stop();
+			server = null;
+		}
+	}
+
+	public int getPort() {
+		return port;
+	}
+}
