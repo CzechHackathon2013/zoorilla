7453ce6e2d120b7068272e2c84c90c13aec048f5
pcipov <pcipov@kerio.com>
2013-10-19 04:29:07 +0200

    removing junk

--- zoorilla-server/src/main/java/cz/hack/zoorilla/ChildrenServlet.java
+++ zoorilla-server/src/main/java/cz/hack/zoorilla/ChildrenServlet.java
@@ -1,27 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package cz.hack.zoorilla;
 
 import com.google.common.base.Charsets;
-import com.google.common.io.ByteStreams;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.nio.charset.Charset;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.zookeeper.KeeperException;
 import org.json.JSONStringer;
-import org.json.JSONWriter;
 
 /**
  *
--- zoorilla-server/src/main/java/cz/hack/zoorilla/NodeServlet.java
+++ zoorilla-server/src/main/java/cz/hack/zoorilla/NodeServlet.java
@@ -1,24 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package cz.hack.zoorilla;
 
-import com.google.common.io.ByteStreams;
 import java.io.IOException;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import org.apache.curator.framework.CuratorFramework;
-import org.apache.curator.framework.CuratorFrameworkFactory;
-import org.apache.curator.retry.RetryNTimes;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.data.Stat;
 
