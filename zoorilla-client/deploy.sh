78ac49bac15ad42b154e60edd3accc71b31e20a3
Ondrej Sika <ondrej@ondrejsika.com>
2013-10-23 23:49:59 +0200

    uncommented push cmd in client deploy script

--- zoorilla-client/deploy.sh
+++ zoorilla-client/deploy.sh
@@ -12,7 +12,8 @@ git --git-dir=$gittmp --work-tree="." init
 git --git-dir=$gittmp --work-tree="." remote add origin $GITHUB_REPO
 git --git-dir=$gittmp --work-tree="." add .
 git --git-dir=$gittmp --work-tree="." commit -a -m "Automatic build"
-# git --git-dir=$gittmp push origin master --force
-# git --git-dir=$gittmp push origin master:gh-pages --force
+git --git-dir=$gittmp push origin master --force
+git --git-dir=$gittmp push origin master:gh-pages --force
 
 rm $gittmp -rf
+
